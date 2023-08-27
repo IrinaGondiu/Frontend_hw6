d38533879ab35ea1bd3ff933237311a59c252ad8
IrinaGondiu <137884439+IrinaGondiu@users.noreply.github.com>
2023-08-27 22:58:44 +0200

    Add files via upload
    fe hw6

---
+++ script.js
@@ -0,0 +1,45 @@
+// Написать программу, которая через prompt считывает число и выводит в консоль число равное 10% от введенного числа
+// 
+
+// let num = +prompt('write a number')
+// console.log(num*0.1);
+
+//===================================================================
+
+// Написать программу, которая получает два числа и выводит наименьшее
+// 
+// let secondNumber = +prompt("Enter a first number")
+// let firstNumber = +prompt("Enter a second number")
+// console.log(Math.min(secondNumber, firstNumber))
+
+//====================================================================
+
+// Написать программу, которая считывает через prompt число и выводит одно из следующих сообщений: ‘Число меньше 100’, ‘Число больше 100’ или ‘Число равно 100’
+
+
+// let number = +prompt('Input the number')
+// if(number<100){
+  // console.log('The number is smaller then 100');
+// } else if(number>100){
+// console.log('The number is greater then 100')
+// }else if(number ===100){
+  // console.log('Your number is 100');
+// }else{
+  // console.log('Invalid');
+// }
+
+//=======================================================================
+
+//Написать программу, которая запрашивает у пользователя его имя и возраст (в годах) и выводит в консоль сообщение ‘Hello, <name>’, если пользователь совершеннолетний, или ‘Hi, <name>’, если пользователь несовершеннолетний.
+
+
+// const name = prompt('Enter your name')
+// const age = +prompt('Enter your age')
+// if(age>=18){
+  // console.log(`Hello, ${name}`);
+// }else if(age <18 && age > 0){
+  // console.log(`Hi, ${name}`);
+// }else if(age < 0){
+  // console.log('Invalid age');
+// }
+// 
